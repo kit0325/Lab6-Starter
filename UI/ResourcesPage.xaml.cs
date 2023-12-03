b42aca841342afe0c4bbc261716ed30f7d3e66ab
mprogers <mprogers@users.noreply.github.com>
2023-12-02 18:51:00 -0600

    Added logout functionality, replaced Airports button text with icons
    Added a +, pencil and logout icon to the Airports page to make it look better
    It looks great on iOS now, but on Android the icons are showing up as black on
    a purple background. Also added some fonts, but didn't actually use them as on
    iOS they were showing up comically large.
    
     Changes to be committed:
    	modified:   App.xaml
    	modified:   Lab6-Starter.csproj
    	modified:   MauiProgram.cs
    	new file:   Resources/Fonts/Font Awesome 6 Free-Solid-900.otf
    	new file:   Resources/Images/logout.png
    	deleted:    Resources/Images/magnifyingglasssolid.svg
    	new file:   Resources/Images/pencil.png
    	new file:   Resources/Images/plus.png
    	new file:   Resources/Styles/FancyIcons.xaml
    	new file:   Resources/Styles/Icons.xaml
    	modified:   UI/AirportsPage.xaml
    	modified:   UI/AirportsPage.xaml.cs
    	modified:   UI/LoginPage.xaml
    	modified:   UI/Map.xaml

--- UI/ResourcesPage.xaml.cs
+++ UI/ResourcesPage.xaml.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 namespace Lab6_Starter;
 public partial class ResourcesPage : ContentPage
 {
-	public ResourcesPage()
-	{
-  
+    public ResourcesPage()
+    {
 
-            InitializeComponent();
 
-            // Create a list of URLs
-            List<string> urlList = new List<string>
+        InitializeComponent();
+
+        // Create a list of URLs
+        List<string> urlList = new List<string>
             {
                 "https://wisconsindot.gov/Pages/travel/air/pilot-info/flywi.aspx",
                 "http://wiama.org/",
@@ -20,7 +20,18 @@ public partial class ResourcesPage : ContentPage
 
             };
 
-            collectionView.ItemsSource = urlList;
-        
+        collectionView.ItemsSource = urlList;
+
+    }
+
+    /// <summary>
+    /// This is the event handler for the Logout button. All it does is bring us back to the Login page
+    /// </summary>
+    /// <param name="sender">button that triggered this</param>
+    /// <param name="e">no events worth worrying about</param>
+    void Logout_Clicked(System.Object sender, System.EventArgs e)
+    {
+        //MauiProgram.BusinessLogic.Logout();
+        Application.Current.MainPage = new LoginPage();
     }
 }
