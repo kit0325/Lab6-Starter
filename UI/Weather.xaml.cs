2ae15d8b4ad35a2d72c076467bdd5e4529bf5d1e
joshua-t-hill <145697719+joshua-t-hill@users.noreply.github.com>
2023-12-03 14:40:16 -0600

    Removed UI elements that were completely visual and had no actual function (basically anything not directly related to the TAF/METAR API); made it so phone keyboard closes upon tapping the "search" button.

--- UI/Weather.xaml.cs
+++ UI/Weather.xaml.cs
@@ -1,19 +1,22 @@
-﻿using Lab6_Starter.Model;
+﻿using CommunityToolkit.Maui.Core.Platform;
+using Lab6_Starter.Model;
 
 
- namespace Lab6_Starter;
+namespace Lab6_Starter;
 
- public partial class Weather : ContentPage
- {
-     public Weather()
-     {
-         InitializeComponent();
-     }
+public partial class Weather : ContentPage
+{
+    public Weather()
+    {
+        InitializeComponent();
+    }
 
-     public void OnSearch_Clicked (object sender, EventArgs e)
-     {
-         MetarLabel.Text = Meteorologist.GetMetar(entry.Text);
-         TafLabel.Text = Meteorologist.GetTaf(entry.Text);
-     }
+    public void OnSearch_Clicked(object sender, EventArgs e)
+    {
+        _ = entry.HideKeyboardAsync(CancellationToken.None); //close the keyboard after searching
 
- }
+        MetarLabel.Text = Meteorologist.GetMetar(entry.Text);
+        TafLabel.Text = Meteorologist.GetTaf(entry.Text);
+    }
+
+}
